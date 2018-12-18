6444926477326cd7b47ed11d07e04fb17c0850d7
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2018-12-18 11:26:53 +0000

    variable creation

---
+++ app.js
@@ -0,0 +1,12 @@
+// Define UI Vars
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+
+// Load all event listeners
+function loadEventListeners() {
+    // Add task event
+}
